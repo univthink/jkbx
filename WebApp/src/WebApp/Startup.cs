c44d475eb47a13062a40f815cc0cb11912bdae38
iteam-software <mlynam@iteamnm.com>
2015-07-18 19:24:59 -0600

    Cleaned startup class

--- WebApp/src/WebApp/Startup.cs
+++ WebApp/src/WebApp/Startup.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 
 namespace WebApp
 {
-    public class Startup
+  public class Startup
+  {
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-
-        }
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+
+    }
+  }
 }
